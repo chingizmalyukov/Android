0c569464e6f11a006fe57bc8c0e2f2d3587359ac
Chingiz <killerban4@gmail.com>
2023-10-03 18:47:54 +0300

    test commit

---
+++ BusCounter/app/src/main/java/com/example/buscounter/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.buscounter
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
+import com.example.buscounter.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var counter = 0
+        binding.buttonMinus.isEnabled = false
+        binding.textPassengers.text = counter.toString()
+        binding.buttonPlus.setOnClickListener {
+            binding.buttonMinus.isEnabled = true
+            binding.textStatus.setTextColor(Color.BLUE)
+            if (counter >= 49) {
+                counter = 50
+                binding.textReset.visibility = View.VISIBLE
+                binding.textStatus.text = "Too many passengers"
+                binding.textStatus.setTextColor(Color.RED)
+            } else {
+                counter++
+                binding.textStatus.text = "passenger seats left ${50 - counter}"
+            }
+            binding.textPassengers.text = counter.toString()
+        }
+        binding.buttonMinus.setOnClickListener {
+            binding.textStatus.setTextColor(Color.BLUE)
+            if (counter == 1) {
+                counter = 0
+                binding.textStatus.text = "All seats are free"
+                binding.textStatus.setTextColor(Color.GREEN)
+                binding.buttonMinus.isEnabled = false
+            } else {
+                counter -= 1
+                binding.textStatus.text = "passenger seats left ${50 - counter}"
+            }
+            binding.textPassengers.text = counter.toString()
+        }
+        binding.textReset.setOnClickListener {
+            counter = 0
+            binding.buttonMinus.isEnabled = false
+            binding.textReset.visibility = View.INVISIBLE
+            binding.textStatus.text = "All seats are free"
+            binding.textStatus.setTextColor(Color.GREEN)
+            binding.textPassengers.text = counter.toString()
+        }
+    }
+}
---
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        var counter = 0
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.textView.text = counter.toString()
+
+        binding.plusOneButton.setOnClickListener {
+            if (counter == 5) {
+                counter = 0
+                binding.button.visibility = View.VISIBLE
+            } else {
+                counter++
+                binding.textView.text = counter.toString()
+            }
+        }
+
+        binding.button.setOnClickListener {
+            binding.textView.text = "And now you are GAY!"
+            binding.plusOneButton.isEnabled = false
+        }
+    }
+}
